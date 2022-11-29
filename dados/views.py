6a4f2319f01f9ba303956eb80cde9c237d23456e
Jamily Lima <jamilylima70@gmail.com>
2022-11-29 13:56:33 -0300

    feat: project adjustments

--- dados/views.py
+++ dados/views.py
@@ -1,25 +1,20 @@
 from django.shortcuts import render
-from .forms import UploadFileForm
-from .models import Dados
-
-
-
+from django.core.files.storage import FileSystemStorage
 
 
 def uploadfile(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save(commit=False)
-            # teste = request.FILES['file']
-            uploaded_file = form.save()
-            return render(request, 'dados/form.html', {
-            'uploaded_file': uploaded_file
-            })
-
-            
-    else:
-        form = UploadFileForm()
-    return render(request, "dados/form.html", {'form':form})
-
     
+    if request.method == 'POST':
+        request_file = request.FILES['document'] if 'document' in request.FILES else None
+        if request_file:
+            fs = FileSystemStorage()
+            file = fs.save(request_file.name, request_file)
+            fileurl = fs.url(file)
+            return render(request, "dados/form.html",{'fileurl': fileurl})
+            
+    return render(request, "dados/form.html")
+
+
+
+   
+
